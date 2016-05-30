3bae7e9113b38fbe0fcb6b747cf9ce22ceb105e5
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-05-30 14:02:13 -0700

    Update mynumber.lua

--- plugins/mynumber.lua
+++ plugins/mynumber.lua
@@ -1,12 +1,12 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+989017040813", "AMOUR", "Xface", ok_cb, false)
+send_contact(get_receiver(msg), "+989017040813", "AMOUR", "MR_ViRuS", ok_cb, false)
 end
 
 return {
 patterns = {
-"^!mynumber$"
+"^myno$"
 
 },
 run = run
